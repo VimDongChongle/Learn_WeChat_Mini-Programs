e5ba3a3ab099db55f111ea2fca395f82ce0a547f
VimDongChongle <dongchongle@gmail.com>
2018-12-15 22:38:36 +0800

    181215-1

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,19 @@
 //index.js
 //获取应用实例
-Page({})
+Page({
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success(res) {
+        console.log(res.data);
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        console.log(temp, weather);
+      }
+    })
+  }
+})
